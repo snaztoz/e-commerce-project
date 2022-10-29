3e0a48fef443b558e7b9610383e4687cb0f98baf
snaztoz <hafidhmn@gmail.com>
2022-10-29 21:44:48 +0700

    feat: add relationship to models

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Subscription extends Model
 {
     use HasFactory;
+
+    public function subscriber()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function plan()
+    {
+        return $this->belongsTo(SubscriptionPlan::class);
+    }
 }
