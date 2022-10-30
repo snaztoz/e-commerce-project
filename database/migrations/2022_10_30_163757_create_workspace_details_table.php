17199fe64cfffff968537af26c5d810cb2116e47
snaztoz <hafidhmn@gmail.com>
2022-10-30 23:41:11 +0700

    feat: add WorkspaceDetail model

---
+++ database/migrations/2022_10_30_163757_create_workspace_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\ProjectStructure;
+use App\Models\Workspace;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workspace_details', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->foreignIdFor(ProjectStructure::class);
+            $table->foreignIdFor(Workspace::class);
+
+            $table->text('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workspace_details');
+    }
+};
