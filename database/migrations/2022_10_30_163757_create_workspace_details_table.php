6baa0f879d637aeab86e140e9a534ee58be12027
snaztoz <hafidhmn@gmail.com>
2022-10-30 23:46:17 +0700

    style: fix imports ordering

--- database/migrations/2022_10_30_163757_create_workspace_details_table.php
+++ database/migrations/2022_10_30_163757_create_workspace_details_table.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Models\ProjectStructure;
-use App\Models\Workspace;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
+use App\Models\ProjectStructure;
+use App\Models\Workspace;
 
 return new class extends Migration
 {
