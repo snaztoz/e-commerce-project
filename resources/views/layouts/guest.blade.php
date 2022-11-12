dbcb9e02eb82135ce0ea9bc37d1a0252df7e46cf
snaztoz <hafidhmn@gmail.com>
2022-11-12 14:17:20 +0700

    style: fix template files styles

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,8 +15,8 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
   <body>
-      <div class="font-sans text-gray-900 antialiased">
-          {{ $slot }}
-      </div>
+    <div class="font-sans text-gray-900 antialiased">
+      {{ $slot }}
+    </div>
   </body>
 </html>
