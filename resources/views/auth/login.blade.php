9ee924e6db1cbccf3b2d9a226d3bf3ec00cea34f
snaztoz <hafidhmn@gmail.com>
2022-11-12 19:06:50 +0700

    feat: add new login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,61 +1,47 @@
-<x-guest-layout>
-  <x-auth-card>
-    <x-slot name="logo">
-      <a href="/">
-        <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-      </a>
-    </x-slot>
+@extends('layouts.base')
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
+@section('content')
+<div class="row mt-5 justify-content-center">
+  <div class="card m-2 col col-md-6 col-lg-5">
+    <form method="POST" action="{{ route('login') }}" class="card-body"
+        novalidate>
       @csrf
 
-      <!-- Email Address -->
-      <div>
-        <x-input-label for="email" :value="__('Email')" />
-
-        <x-text-input id="email" class="block mt-1 w-full" type="email"
-            name="email" :value="old('email')" required autofocus />
-
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+      <div class="mb-2">
+        <label for="email" class="form-label">{{ __('Email') }}</label>
+        <input type="email" class="form-control @error('email') is-invalid
+            @enderror" id="email" name="email"
+            placeholder="your-email@email.com" required>
+        <div class="invalid-feedback">
+          @error('email') {{ $message }} @enderror
+        </div>
       </div>
 
-      <!-- Password -->
-      <div class="mt-4">
-        <x-input-label for="password" :value="__('Password')" />
-
-        <x-text-input id="password" class="block mt-1 w-full"
-            type="password" name="password" required
-            autocomplete="current-password" />
-
-        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+      <div class="mb-3">
+        <label for="password" class="form-label">{{ __('Password') }}</label>
+        <input type="password" class="form-control @error('email') is-invalid
+            @enderror" id="password" name="password" placeholder="your-password"
+            required>
+        <div class="invalid-feedback">
+          @error('password') {{ $message }} @enderror
+        </div>
       </div>
 
-      <!-- Remember Me -->
-      <div class="block mt-4">
-        <label for="remember_me" class="inline-flex items-center">
-          <input id="remember_me" type="checkbox" class="rounded
-              border-gray-300 text-indigo-600 shadow-sm
-              focus:border-indigo-300 focus:ring focus:ring-indigo-200
-              focus:ring-opacity-50" name="remember">
-          <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+      <div class="form-check mb-3">
+        <input class="form-check-input" type="checkbox" id="remember">
+        <label class="form-check-label" for="remember">
+          {{ __('Remember me') }}
         </label>
       </div>
 
-      <div class="flex items-center justify-end mt-4">
-        @if (Route::has('password.request'))
-          <a class="underline text-sm text-gray-600 hover:text-gray-900"
-              href="{{ route('password.request') }}">
-            {{ __('Forgot your password?') }}
-          </a>
-        @endif
+      <button type="submit" class="btn btn-primary mb-3 w-100">Login</button>
 
-        <x-primary-button class="ml-3">
-          {{ __('Log in') }}
-        </x-primary-button>
-      </div>
+      <p class="text-center">
+        <a href="{{ route('register') }}">
+          {{ __('Doesn\'t have an account?') }}
+        </a>
+      </p>
     </form>
-  </x-auth-card>
-</x-guest-layout>
+  </div>
+</div>
+@endsection
